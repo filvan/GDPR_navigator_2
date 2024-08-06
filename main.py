83e50a2c4b3d9fdf52501619ddd525fce0483aea
Esteban Garcia Taquez <esgartaq@gmail.com>
2024-08-06 10:27:11 -0500

    imported mathplot.pyplot
    is used for the visualization of networkx

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import re
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 class LegalText:
     def __init__(self, name, translated=False, references=None):
